ebc0767bfc6c187c91cce740e994ce0dd3740c81
kahnjw <jarrod.kahn@gmail.com>
2015-04-01 06:55:56 -0700

    Use symbolic contants where possible

--- chapter_1/degrees.c
+++ chapter_1/degrees.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define LOWER 0     /* lower limit of table */
+#define UPPER 300   /* upper limit */
+#define STEP 20     /* step size */
+
 /* print Fahrenheit-Celcius table
  * for fahr = 0, 20, ..., 30 
  */
@@ -9,15 +13,11 @@ main()
     float fahr, celsius;
     int lower, upper, step;
 
-    lower = 0;      /* lower limit of temp table */
-    upper = 300;    /* uperr limit */
-    step = 20;      /* step size */
+    fahr = LOWER;
 
-    fahr = lower;
-
-    while(fahr <= upper) {
+    while(fahr <= UPPER) {
         celsius = (5.0/9.0) * (fahr-32);
         printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+        fahr = fahr + STEP;
     }
 }
