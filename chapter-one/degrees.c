206ae8b2002e213fab643cae2a10ec8065f70357
kahnjw <jarrod.kahn@gmail.com>
2015-03-31 19:59:09 -0700

    Improve degrees.c output and use floats over its

--- chapter-one/degrees.c
+++ chapter-one/degrees.c
@@ -6,7 +6,7 @@
 
 main()
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;      /* lower limit of temp table */
@@ -16,8 +16,8 @@ main()
     fahr = lower;
 
     while(fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
